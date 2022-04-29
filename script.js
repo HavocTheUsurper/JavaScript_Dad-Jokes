0a65497604d7e3a88a880d92e96dccfd8539bf1d
Jeremy Ball <98829121+HavocTheUsurper@users.noreply.github.com>
2022-04-29 09:13:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,33 +6,32 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-async function generateJoke () {
-	const config = {
-		headers: {
-			Accept: 'application/json',
-		},
-	}
-	
-	const res = await fetch('https://icanhazdadjoke.com', config)
-		
-	const data = await res.json()
-	
-	jokeEl.innerHTML = data.joke
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
 
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
 
-
-//function generateJoke () {
-//	const config = {
-//		headers: {
-//			Accept: 'application/json',
-//		},
-//	}
-	
-//	fetch('https://icanhazdadjoke.com', config)
-//		.then((res) =>res.json())
-//		.then((data) => {
-//		jokeEl.innerHTML = data.joke
-		
-//	})
-//}
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
